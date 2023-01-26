cd85ee7935c8ac65803152dd5043812f5dbc30d2
ladley <reus7042@gmail.com>
2023-01-26 16:47:45 +0900

    AM-42 remove none-use function, components

--- src/pages/User/Add.js
+++ src/pages/User/Add.js
@@ -4,15 +4,10 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
-import OtherDatePicker from 'react-datepicker';
-
-import setHours from "date-fns/setHours";
-import setMinutes from "date-fns/setMinutes";
-import moment from 'moment'
 
 import { useNavigate } from 'react-router-dom'
 
-import { Box, Button, Chip, Card, CardContent, Divider, TextField, Typography } from '@mui/material';
+import { Box, Button, Card, CardContent, Divider, TextField } from '@mui/material';
 
 import { db } from '../../firebase'
 
@@ -21,55 +16,48 @@ startOfToday.setHours(0)
 startOfToday.setMinutes(0)
 startOfToday.setSeconds(0)
 
-export const DEFAULT_REGULAR_SCHEDULR = [
+export const DEFAULT_REGULAR_SCHEDULE = [
   {
     day: 'Sunday',
     korLabel: '일',
-    startTM: startOfToday,
-    endTM: startOfToday,
-    use: false
+    use: false,
+    schedules: []
   },
   {
     day: 'Monday',
     korLabel: '월',
-    startTM: startOfToday,
-    endTM: startOfToday,
-    use: false
+    use: false,
+    schedules: []
   },
   {
     day: 'Tuesday',
     korLabel: '화',
-    startTM: startOfToday,
-    endTM: startOfToday,
-    use: false
+    use: false,
+    schedules: []
   },
   {
     day: 'Wednesday',
     korLabel: '수',
-    startTM: startOfToday,
-    endTM: startOfToday,
-    use: false
+    use: false,
+    schedules: []
   },
   {
-    day: 'THursday',
+    day: 'Thursday',
     korLabel: '목',
-    startTM: startOfToday,
-    endTM: startOfToday,
-    use: false
+    use: false,
+    schedules: []
   },
   {
     day: 'Friday',
     korLabel: '금',
-    startTM: startOfToday,
-    endTM: startOfToday,
-    use: false
+    use: false,
+    schedules: []
   },
   {
     day: 'Saturday',
     korLabel: '토',
-    startTM: startOfToday,
-    endTM: startOfToday,
-    use: false
+    use: false,
+    schedules: []
   },
 ]
 
@@ -78,40 +66,7 @@ export default function Add() {
   const [name, setName] = React.useState('')
   const [birth, setBirth]  = React.useState(new Date())
   const [phone, setPhone] = React.useState('')
-  const [regularSchedule, setRegularSchedule] = React.useState(DEFAULT_REGULAR_SCHEDULR)
   const navigate = useNavigate()
-
-  const handleClickDay = (index) => {
-    setRegularSchedule(prev => [
-      ...prev.slice(0, index),
-      {
-        ...prev[index],
-        use: !prev[index].use
-      },
-      ...prev.slice(index + 1)
-    ])
-  }
-
-  const handleClickTime = (index, type, time) => {
-    setRegularSchedule(prev => [
-      ...prev.slice(0, index),
-      {
-        ...prev[index],
-        [type]: time
-      },
-      ...prev.slice(index + 1)
-    ])
-    if (type === 'startTM') {
-      setRegularSchedule(prev => [
-        ...prev.slice(0, index),
-        {
-          ...prev[index],
-          'endTM': moment(time).add('4', 'hour').toDate()
-        },
-        ...prev.slice(index + 1)
-      ])
-    }
-  }
 
   const handleAddStudent = async () => {
     try {
@@ -122,17 +77,12 @@ export default function Add() {
 
       const addDataRes = await db.collection('Student').doc(addedStudentId).set({
         id: addedStudentId,
-        regularSchedule: regularSchedule.map(schedule => ({
-          ...schedule,
-          startTM: moment(schedule.startTM).format('HH:mm').toString(),
-          endTM: moment(schedule.endTM).format('HH:mm').toString()
-        })),
-        name, birth, phone
+        regularSchedule: DEFAULT_REGULAR_SCHEDULE, name, birth, phone
       })
 
       console.log('data add success:', addDataRes)
 
-      navigate('/dashboard/user')
+      navigate('/dashboard/student')
     } catch(e) {
       console.error(e)
     }
@@ -173,72 +123,7 @@ export default function Add() {
                 renderInput={(params) => <TextField {...params} />}
               />
             </LocalizationProvider>
-            <Card
-              style={{
-                overflow: 'visible'
-              }}
-            >
-              <CardContent>
-                <Typography variant="h4" gutterBottom>
-                  정기 스케줄
-                </Typography>
-                <DayPicker>
-                  { regularSchedule.map((value, index) =>
-                    <Chip
-                      key={`${value.day} use selector-${index}`}
-                      color='primary'
-                      label={value.korLabel}
-                      variant={value.use ? 'filled' : 'outlined'}
-                      clickable
-                      onClick={() => handleClickDay(index)}
-                    />
-                  )}
-                </DayPicker>
-                { regularSchedule.map((value, index) => {
-                  let strColor = 'default'
-                  if (value.day === 'Sunday') strColor = 'error'
-                  else if (value.day === 'Saturday') strColor = 'primary'
-                  return <DayTimeWrap key={`${value.day} selector-${index}`}>
-                    <Typography variant="h5" gutterBottom color={strColor}>
-                      {value.korLabel}요일
-                    </Typography>
-                    <TimePickerWrap>
 
-                      <OtherDatePicker
-                        placeholderText='시작시간'
-                        selected={value.startTM}
-                        onChange={(time) => handleClickTime(index, 'startTM', time)}
-                        showTimeSelect
-                        showTimeSelectOnly
-                        timeFormat="HH:mm a"
-                        timeIntervals={30}
-                        timeCaption="시작시간"
-                        dateFormat="hh:mm a"
-                        disabled={!value.use}
-                        minTime={setHours(setMinutes(new Date(), 0), 9)}
-                        maxTime={setHours(setMinutes(new Date(), 0), 22)}
-                      />
-                      ~
-                      <OtherDatePicker
-                        placeholderText='종료시간'
-                        selected={value.endTM}
-                        onChange={(time) => handleClickTime(index, 'endTM', time)}
-                        showTimeSelect
-                        showTimeSelectOnly
-                        timeFormat="HH:mm a"
-                        timeIntervals={30}
-                        timeCaption="종료시간"
-                        dateFormat="hh:mm a"
-                        disabled={!value.use}
-                        minTime={setHours(setMinutes(new Date(), 0), 10)}
-                        maxTime={setHours(setMinutes(new Date(), 0), 23)}
-                      />
-                    </TimePickerWrap>
-                  </DayTimeWrap>
-                }
-                )}
-              </CardContent>
-            </Card>
             <Button
               style={{ margin: 8 }}
               fullWidth
@@ -258,22 +143,4 @@ const FormWrap = styled.div`
   display: flex;
   flex-direction: column;
   gap: 8px;
-`
-
-const DayPicker = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  margin-bottom: 8px;
-`
-
-const DayTimeWrap = styled.div`
-  margin-bottom: 8px;
-`
-
-const TimePickerWrap = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
 `
